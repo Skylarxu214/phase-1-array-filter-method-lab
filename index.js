69ff1f5b26b1aaabba6d8ac472995a01e0ea8236
Tian Xu <xueqingxu0302@gmail.com>
2021-11-27 18:04:51 -0700

    commit tree 0466bc190ebca5eeaedf20207e85157be057853f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(arr , name){
+   const newarr = arr.filter(x => x.toLowerCase() === name.toLowerCase());
+   return newarr 
+ }
+
+    // Code your solution here
+function fuzzyMatch(arr ,str){
+    const newarr = arr.filter(x => x.slice(0,str.length) === str);
+     return newarr;
+}
+
+function matchName(arr , str){
+    const newarr = arr.filter(x => x.name === str );
+    return newarr ;
+}
